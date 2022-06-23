f44fe86f7515ab09d2dad5e7e1f1ab93f80b9641
Obaida Gharbia <obaida.gharbia98@gmail.com>
2022-06-23 19:21:51 +0300

    "Totaly finished with Preprocessing starting with Keras model"

--- Decode_csv.py
+++ Decode_csv.py
@@ -9,22 +9,21 @@ def read_and_decode_orginal(filename, reshape_dims):
     img = tf.io.decode_image( img, channels=3,dtype=tf.dtypes.float32,expand_animations = False)
     # 3. Resize the image to the desired size.
 
-    return tf.image.resize(img, reshape_dims)
+    return tf.image.resize(img, reshape_dims,method='nearest')
 
 def read_and_decode_segmented(filename, reshape_dims,Num_Classes):
 
     # 1. Read the file.
     img = tf.io.read_file(filename)
     # 2. Convert the compressed string to a 3D int tensor.
-    img = tf.io.decode_image( img,channels=1,dtype=tf.dtypes.int32,expand_animations = False)
+    img = tf.io.decode_image( img,channels=1,dtype=tf.dtypes.uint8,expand_animations = False)
     
-    img = tf.image.resize(img, reshape_dims)
+    img = tf.image.resize(img, reshape_dims,method='nearest')
     img = tf.reshape(img,reshape_dims)
 
-    img = tf.image.convert_image_dtype(img, dtype=tf.dtypes.int32)
     # 3. Resize the image to the desired size.
-    img = tf.one_hot(img, Num_Classes, dtype=tf.int32)
-    
+    img = tf.one_hot(img, Num_Classes, dtype=tf.uint8)
+        
     return img
 
 def decode_csv(combined,orginal_path,segmented_path):
